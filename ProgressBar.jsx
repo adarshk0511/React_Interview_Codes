66001ee6bdfb20ac207d303fa97aed103bab9374
adarsh_k_poojary <108194966+adarshk0511@users.noreply.github.com>
2025-11-01 00:00:12 +0530

    Add styles for app layout and progress bar
    Added styles for the app and progress bar components.

---
+++ ProgressBar.jsx
@@ -0,0 +1,80 @@
+import React,{useState, useEffect} from 'react';
+import "./styles.css"
+const ProgressBar = ({value=0}) =>{
+  
+  const [percent, setPercent] = useState(value);
+
+  useEffect(()=>{
+    setPercent(Math.min(100, Math.max(value, 0)));
+  }, [value])
+  
+  return(
+    <div className="progress">
+      <span style={{color: percent> 49 ? "white" :"black"}}>{percent.toFixed()}%</span>
+      <div style={{width: `${percent}%`}}/>
+    </div>  
+  )
+};
+
+const App = () => {
+  
+  const [value, setValue] = useState(0);
+  
+  useEffect(()=>{
+    setInterval(()=>{
+      setValue((val) => val+1);
+    }, 50);
+  }, [])
+  
+  return (
+    <div className="app">
+      <span>Progress Bar</span>
+      <ProgressBar value={value}/>
+    </div>
+  );
+};
+
+export default App;
+
+
+==================  styles.css============================
+
+.app{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 7px;
+}
+
+.progress{
+  height: 20px;
+  width: 300px;
+  background-color: rgb(233, 236, 239);
+  border: 1px solid black;
+  border-radius: 15px;
+  overflow: hidden;
+   position: relative;
+}
+
+.progress div {
+  height: 100%;
+  background-color: #00c251;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+
+.progress span {
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  z-index: 2;
+}
+
+
+
+
+
+
