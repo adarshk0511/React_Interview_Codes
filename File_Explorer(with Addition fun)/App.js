df9a91260f246f81e3176fcacd21b30679246d4b
adarsh_k_poojary <108194966+adarshk0511@users.noreply.github.com>
2025-11-02 00:00:41 +0530

    Add files via upload

---
+++ File_Explorer(with Addition fun)/App.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./styles.css";
+import explorer from "./data/folderData";
+import Folder from "./components/Folder";
+import useTraverseTree from "./hooks/use-traverse-tree";
+
+export default function App() {
+  const [explorerData, setExplorerData] = useState(explorer);
+  const { insertNode } = useTraverseTree();
+
+  const handleInsertNode = (folderId, item, isFolder) => {
+    const finalTree = insertNode(explorerData, folderId, item, isFolder);
+    setExplorerData(finalTree);
+  };
+  return (
+    <div className="app">
+      <Folder handleInsertNode={handleInsertNode} explorer={explorerData} />
+    </div>
+  );
+}
