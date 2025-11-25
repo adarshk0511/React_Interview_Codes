95fe6b8b543a20d12908b7ebc46a3a9a8fe08484
adarsh_k_poojary <108194966+adarshk0511@users.noreply.github.com>
2025-11-26 00:50:32 +0530

    Improve OTP input handling and focus behavior
    Enhanced OTP input component with focus management and input validation.

--- OTP Login/components/otp-input.jsx
+++ OTP Login/components/otp-input.jsx
@@ -1,13 +1,56 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "../styles.css";
 const OtpInput = ({ length = 4, onOtpSubmit = () => {} }) => {
   const [otp, setOtp] = useState(new Array(length).fill(""));
+  const inputRefs = useRef([]);
 
+  useEffect(() => {
+    if (inputRefs.current[0]) {
+      inputRefs.current[0].focus();
+    }
+  }, []);
   console.log(otp);
 
-  const handleChange = () => {};
-  const handleClick = () => {};
-  const handleKeyDown = () => {};
+  const handleChange = (index, e) => {
+    const value = e.target.value;
+    if (isNaN(value)) return;
+
+    const newOtp = [...otp];
+    //only one input per field
+    newOtp[index] = value.substring(value.length - 1);
+    setOtp(newOtp);
+
+    //join all the numbers , if len satisfies then trigger
+    const combinedOtp = newOtp.join("");
+    if (combinedOtp.length == length) onOtpSubmit(combinedOtp);
+
+    //Move to next input if current is filled
+    if (value && index < length - 1 && inputRefs.current[index + 1]) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleClick = (index) => {
+    //Keeping the cursor in the end
+    inputRefs.current[index].setSelectionRange(1, 1);
+
+    //If previous field gets missed
+    if (index > 0 && !otp[index - 1]) {
+      inputRefs.current[otp.indexOf("")].focus();
+    }
+  };
+
+  const handleKeyDown = (index, e) => {
+    if (
+      e.key === "Backspace" &&
+      !otp[index] &&
+      index > 0 &&
+      inputRefs.current[index - 1]
+    ) {
+      //Moving back on backspace
+      inputRefs.current[index - 1].focus();
+    }
+  };
 
   return (
     <div>
@@ -16,6 +59,7 @@ const OtpInput = ({ length = 4, onOtpSubmit = () => {} }) => {
           <input
             key={index}
             type="text"
+            ref={(input) => (inputRefs.current[index] = input)}
             value={value}
             onChange={(e) => handleChange(index, e)}
             onClick={() => handleClick(index)}
