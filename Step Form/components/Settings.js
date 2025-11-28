c9fe9632e7c7f773235aa0fd711b03a5f2f0ef46
adarsh_k_poojary <108194966+adarshk0511@users.noreply.github.com>
2025-11-29 00:04:28 +0530

    Update Settings component to handle theme selection
    Refactor Settings component to manage theme state with radio buttons.

--- Step Form/components/Settings.js
+++ Step Form/components/Settings.js
@@ -1,5 +1,39 @@
-const Settings = () => {
-  return <div>Settings Form</div>;
+const Settings = ({ data, setData, errors }) => {
+  const { theme } = data;
+
+  const handleDataChange = (e) => {
+    setData((preState) => ({
+      ...preState,
+      theme: e.target.name,
+    }));
+  };
+  return (
+    <div>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="dark"
+            checked={theme === "dark"}
+            onChange={handleDataChange}
+          />{" "}
+          Dark
+        </label>{" "}
+      </div>
+
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="light"
+            checked={theme === "light"}
+            onChange={handleDataChange}
+          />{" "}
+          Light
+        </label>{" "}
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
