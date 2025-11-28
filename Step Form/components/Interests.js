38f9b0966e33107abf08b9357591b8dd9616730a
adarsh_k_poojary <108194966+adarshk0511@users.noreply.github.com>
2025-11-29 00:04:52 +0530

    Refactor Interests component to handle checkboxes

--- Step Form/components/Interests.js
+++ Step Form/components/Interests.js
@@ -1,5 +1,56 @@
-const Interests = () => {
-  return <div>Interest Form</div>;
+const Interests = ({ data, setData, errors }) => {
+  const { interests } = data;
+
+  const handleDataChange = (e, name) => {
+    setData((preState) => ({
+      ...preState,
+      interests: e.target.checked
+        ? [...preState.interests, e.target.name]
+        : preState.interests.filter((i) => i !== e.target.name),
+    }));
+  };
+
+  console.log(interests);
+  return (
+    <div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="coding"
+            checked={interests.includes("coding")}
+            onChange={handleDataChange}
+          />{" "}
+          Coding
+        </label>{" "}
+      </div>
+
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="music"
+            checked={interests.includes("music")}
+            onChange={handleDataChange}
+          />{" "}
+          Music
+        </label>{" "}
+      </div>
+
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="react"
+            checked={interests.includes("react")}
+            onChange={handleDataChange}
+          />{" "}
+          React
+        </label>{" "}
+      </div>
+      {errors.interests && <span className="error">{errors.interests}</span>}
+    </div>
+  );
 };
 
 export default Interests;
