7629132c9d622bba2a73b0d1cd63941be5735bf4
adarsh_k_poojary <108194966+adarshk0511@users.noreply.github.com>
2025-11-29 00:05:19 +0530

    Refactor Profile component to handle form data

--- Step Form/components/Profile.js
+++ Step Form/components/Profile.js
@@ -1,5 +1,45 @@
-const Profile = () => {
-  return <div>Profile Form</div>;
+const Profile = ({ data, setData, errors }) => {
+  const { name, email, age } = data;
+
+  const handleDataChange = (e, item) => {
+    setData((prevState) => ({
+      ...prevState,
+      [item]: e.target.value,
+    }));
+  };
+  return (
+    <div>
+      <div>
+        <label htmlFor="">Name: </label>{" "}
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => handleDataChange(e, "name")}
+        />
+        {errors.name && <span className="error">{errors.name}</span>}
+      </div>
+
+      <div>
+        <label htmlFor="">Age: </label>{" "}
+        <input
+          type="number"
+          value={age}
+          onChange={(e) => handleDataChange(e, "age")}
+        />
+        {errors.age && <span className="error">{errors.age}</span>}
+      </div>
+
+      <div>
+        <label htmlFor="">email: </label>{" "}
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => handleDataChange(e, "email")}
+        />
+      </div>
+      {errors.email && <span className="error">{errors.email}</span>}
+    </div>
+  );
 };
 
 export default Profile;
