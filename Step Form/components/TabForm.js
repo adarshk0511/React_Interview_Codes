80444ca6082491aa6599977f752e6a8588f48a97
adarsh_k_poojary <108194966+adarshk0511@users.noreply.github.com>
2025-11-28 23:22:00 +0530

    Enhance TabForm with data handling and validation
    Added state management for form data and validation logic.

--- Step Form/components/TabForm.js
+++ Step Form/components/TabForm.js
@@ -5,10 +5,33 @@ import Settings from "./Settings";
 
 const TabForm = () => {
   const [activeTab, setActiveTab] = useState(0);
+  const [data, setData] = useState({
+    name: "ada",
+    age: "23",
+    email: "ada@gmail.com",
+    interests: ["coding", "music"],
+    theme: "dark",
+  });
   const tabs = [
     {
       name: "Profile",
       component: Profile,
+      validate: () => {
+        const err = {};
+        if (!data.name || data.name.length < 2) {
+          err.name = "Name is not valid";
+        }
+        if (!data.age || data.age < 12) {
+          err.age = "Age is not valid";
+        }
+        if (!data.email || data.email.length < 2) {
+          err.email = "Email is not valid";
+        }
+
+        setErrors(err);
+
+        return err.name || err.age || err.email ? false : true;
+      },
     },
     {
       name: "Interests",
@@ -20,7 +43,20 @@ const TabForm = () => {
     },
   ];
 
+  const [errors, setErrors] = useState({});
+
   const ActiveTabComponent = tabs[activeTab].component;
+
+  const handleNextClick = () => {
+    setActiveTab((prev) => prev + 1);
+  };
+  const handlePrevClick = () => {
+    setActiveTab((prev) => prev - 1);
+  };
+  const handleSubmitClick = () => {
+    //Make an API call
+    console.log(data);
+  };
   return (
     <div>
       <div className="heading-container">
@@ -35,7 +71,17 @@ const TabForm = () => {
         ))}
       </div>
       <div className="tab-body">
-        <ActiveTabComponent />
+        <ActiveTabComponent data={data} setData={setData} errors={errors} />
+      </div>
+
+      <div>
+        {activeTab > 0 && <button onClick={handlePrevClick}>Prev</button>}
+        {activeTab < tabs.length - 1 && (
+          <button onClick={handleNextClick}>Next</button>
+        )}
+        {activeTab === tabs.length - 1 && (
+          <button onClick={handleSubmitClick}>Submit</button>
+        )}
       </div>
     </div>
   );
