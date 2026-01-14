d63241c829a6c2e200908be0d9f15704c95534a5
adarsh_k_poojary <108194966+adarshk0511@users.noreply.github.com>
2026-01-14 22:59:26 +0530

    Implement keyboard navigation in dropdown component
    Added keyboard navigation for dropdown results.

--- Dropbow with Search/components/dropdown.jsx
+++ Dropbow with Search/components/dropdown.jsx
@@ -4,10 +4,33 @@ import { options } from "../data/optionsList";
 const Dropdown = () => {
   const [input, setInput] = useState("");
   const [showResults, setShowResults] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState(-1);
 
   const filteredResult = options.filter((opt) =>
     opt.toLowerCase().includes(input.toLowerCase())
   );
+
+  const handleKeyDown = (e) => {
+    if (!showResults) return;
+
+    if (e.key === "ArrowDown") {
+      setHighlightedIndex((prev) =>
+        Math.min(prev + 1, filteredResult.length - 1)
+      );
+    }
+
+    if (e.key === "ArrowUp") {
+      setHighlightedIndex((prev) => Math.max(prev - 1, 0));
+    }
+    if (e.key === "Enter" && highlightedIndex >= 0) {
+      setInput(filteredResult[highlightedIndex]);
+      setShowResults(false);
+    }
+
+    if (e.key === "Escape") {
+      setShowResults(false);
+    }
+  };
 
   return (
     <div>
@@ -17,6 +40,7 @@ const Dropdown = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         onFocus={() => setShowResults(true)}
+        onKeyDown={(e) => handleKeyDown(e)}
       />
       {showResults && (
         <div
@@ -25,8 +49,11 @@ const Dropdown = () => {
         >
           {filteredResult.length === 0 && <p>No results</p>}
 
-          {filteredResult.map((op) => (
-            <p
+          {filteredResult.map((op, index) => (
+            <span
+              className={`result ${
+                index === highlightedIndex ? "highlighted" : ""
+              }`}
               key={op}
               onClick={() => {
                 setInput(op);
@@ -34,7 +61,7 @@ const Dropdown = () => {
               }}
             >
               {op}
-            </p>
+            </span>
           ))}
         </div>
       )}
