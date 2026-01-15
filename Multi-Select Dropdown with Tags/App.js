904a8b6b763034f2e17cf6c6b5e5b74a51fc3f0c
adarsh_k_poojary <108194966+adarshk0511@users.noreply.github.com>
2026-01-15 22:18:47 +0530

    Add files via upload

---
+++ Multi-Select Dropdown with Tags/App.js
@@ -0,0 +1,11 @@
+import Dropdown from "./components/dropdown";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Multi-Select Dropdown with Tags</h1>
+      <Dropdown />
+    </div>
+  );
+}
---
+++ Multi-Select Dropdown with Tags/components/dropdown.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { options } from "../data/optionsList";
+
+const Dropdown = () => {
+  const [input, setInput] = useState("");
+  const [showResults, setShowResults] = useState(true);
+  const [highlightedIndex, setHighlightedIndex] = useState(-1);
+  const [tags, setTags] = useState([]);
+
+  const filteredList = options.filter((opt) =>
+    opt.toLowerCase().includes(input.toLowerCase())
+  );
+
+  const handleKeyDown = (e) => {
+    if (!showResults) return;
+
+    if (e.key === "ArrowDown") {
+      setHighlightedIndex((prev) =>
+        Math.min(prev + 1, filteredList.length - 1)
+      );
+    }
+
+    if (e.key === "ArrowUp") {
+      setHighlightedIndex((prev) => Math.max(prev - 1, 0));
+    }
+    if (e.key === "Enter" && highlightedIndex >= 0 && input.length > 0) {
+      setInput(filteredList[highlightedIndex]);
+
+      if (tags.includes(filteredList[highlightedIndex])) return;
+      setTags((prev) => [...prev, filteredList[highlightedIndex]]);
+    }
+
+    if (e.key === "Escape") {
+      setShowResults(false);
+    }
+  };
+
+  const deleteTag = (index) => {
+    setTags((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  const addTag = (index) => {
+    if (tags.includes(filteredList[index])) return;
+    setTags((prev) => [...prev, filteredList[index]]);
+  };
+
+  return (
+    <div className="outer">
+      <div>
+        <input
+          type="text"
+          className="search-input"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => handleKeyDown(e)}
+          onFocus={() => setShowResults(true)}
+        />
+        {showResults && (
+          <div className="results-container">
+            {filteredList.map((opt, index) => {
+              return (
+                <span
+                  key={opt}
+                  onClick={() => addTag(index)}
+                  className={`result ${
+                    index === highlightedIndex ? "highlighted" : ""
+                  }`}
+                >
+                  {opt}
+                </span>
+              );
+            })}
+          </div>
+        )}
+      </div>
+      <div className="tags-container">
+        <h3>Tags</h3>
+        {tags.map((tag, index) => {
+          return (
+            <span key={tag} className="tag">
+              {tag} <button onClick={() => deleteTag(index)}>❌</button>
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
